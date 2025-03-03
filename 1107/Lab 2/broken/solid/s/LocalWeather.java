107195c82ceae5271ce6307e4faa9d24fa95a274
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-03-03 18:22:47 +0200

    Create LocalWeather.java

---
+++ 1107/Lab 2/broken/solid/s/LocalWeather.java
@@ -0,0 +1,24 @@
+class LocalWeather{
+
+    //TO DO
+
+    public LocalWeather() {
+
+    }
+
+    public int getCurrentTemp() {
+
+        double latitude = getLatitude();
+        double longitude = getLongitude();
+
+        return 0;
+    }
+    public double getLatitude() {
+        //TO DO
+        return 0;
+    }
+    public double getLongitude() {
+        //TO DO
+        return 0;
+    }
+}
