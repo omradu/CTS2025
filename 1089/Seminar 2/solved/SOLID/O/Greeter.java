f016826c68e4931d97971db7b31c5a22b5c248b8
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-03-05 21:43:56 +0200

    Create Greeter.java

---
+++ 1089/Seminar 2/solved/SOLID/O/Greeter.java
@@ -0,0 +1,11 @@
+public class Greeter {
+    private Personality personality;
+
+    public Greeter(Personality personality) {
+        this.personality = personality;
+    }
+
+    public String greet() {
+        return this.personality.greet();
+    }
+}
