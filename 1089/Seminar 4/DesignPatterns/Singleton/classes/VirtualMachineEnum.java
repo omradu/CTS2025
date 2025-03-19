4d0d0d15deb63d2fe36fc1075d0c4eb3c088d63c
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-03-19 20:18:01 +0200

    Create VirtualMachineEnum.java

---
+++ 1089/Seminar 4/DesignPatterns/Singleton/classes/VirtualMachineEnum.java
@@ -0,0 +1,10 @@
+public enum VirtualMachineEnum {
+    INSTANCE; 
+
+    private int connectionPort;
+
+    public void doSomething() {
+        System.out.println("This is a singleton");
+    }
+
+}
