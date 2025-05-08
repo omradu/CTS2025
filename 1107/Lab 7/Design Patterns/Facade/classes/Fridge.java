90fa219ca90c25584f9485fd2df04b28f42c574e
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-08 19:04:48 +0300

    Create Fridge.java

---
+++ 1107/Lab 7/Design Patterns/Facade/classes/Fridge.java
@@ -0,0 +1,39 @@
+public class Fridge {
+    private boolean isDoorOpen = false;
+    protected boolean isExtraFreezeOn = false;
+    private boolean isSpeakerOn = false;
+    private int itemCount = 0;
+
+    public void openDoor()
+    {
+        isDoorOpen = true;
+    }
+
+    public void closeDoor()
+    {
+        isDoorOpen = false;
+    }
+
+    public void turnExtraFreezeOn() {
+
+        isExtraFreezeOn = true;
+    }
+
+    public void turnExtraFreezeOff() {
+        isExtraFreezeOn = false;
+    }
+
+    public void turnSpeakerOn() {
+
+        isSpeakerOn = true;
+    }
+
+    public void turnSpeakerOff() {
+        isSpeakerOn = false;
+    }
+
+    public void setItemCount(int iCount) {
+        itemCount = iCount;
+    }
+
+}
