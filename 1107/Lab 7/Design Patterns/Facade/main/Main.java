d1524da83b568d9714db0ec2221320df3b84c3f3
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-08 19:16:42 +0300

    Create Main.java

---
+++ 1107/Lab 7/Design Patterns/Facade/main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Marcela Marcela = new Marcela(new Fridge(), new Lights(), new Lights(), new Lights(), new WashingMachine());
+        
+        Marcela.cookingMode();
+        Marcela.goodbye();
+        Marcela.welcomeBack();
+    }
+}
