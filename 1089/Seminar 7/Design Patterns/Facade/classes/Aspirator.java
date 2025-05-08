ea7d5998ad0adb02e8195bb9385668778fcda07e
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-08 20:13:17 +0300

    Create Aspirator.java

---
+++ 1089/Seminar 7/Design Patterns/Facade/classes/Aspirator.java
@@ -0,0 +1,11 @@
+public class Aspirator {
+    protected boolean estePornit = false;
+
+    public void porneste() {
+        estePornit = true;
+    }
+
+    public void opreste() {
+        estePornit = false;
+    }
+}
