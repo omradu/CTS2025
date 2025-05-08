bcc9019764f808149fd9c3a82f09561d435f51f8
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-08 20:12:54 +0300

    Create Frigider.java

---
+++ 1089/Seminar 7/Design Patterns/Facade/classes/Frigider.java
@@ -0,0 +1,24 @@
+public class Frigider {
+    private boolean esteUsaDeschisa = false;
+    protected boolean esteExtraFreezePornit = false;
+
+    public void deschide()
+    {
+        esteUsaDeschisa = true;
+    }
+
+    public void inchide()
+    {
+        esteUsaDeschisa = false;
+    }
+
+    public void pornesteExtraFreeze() {
+
+        esteExtraFreezePornit = true;
+    }
+    
+    public void opresteExtraFreeze() {
+
+        esteExtraFreezePornit = false;
+    }
+}
