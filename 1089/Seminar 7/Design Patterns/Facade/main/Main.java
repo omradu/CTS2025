dc8d3f700a4306429b7ccd1ae3f232029a0d13d1
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-08 20:19:55 +0300

    Create Main.java

---
+++ 1089/Seminar 7/Design Patterns/Facade/main/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Alexa Alexa = new Alexa(new Frigider(), new Lumini(), new Lumini(), new Lumini(), new MasinaSpalat(), new Aspirator());
+
+        Alexa.bunaDimineata();
+        Alexa.sporLaGatit();
+        Alexa.noapteBuna();
+
+    }
+}
