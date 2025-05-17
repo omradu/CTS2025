abbea921f5dc43d935a02c7b0d035cb79fa35c84
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-17 19:01:48 +0300

    Create CharacterContext.java

---
+++ 1107/Lab 9/Design Patterns/Flyweight/classes/CharacterContext.java
@@ -0,0 +1,11 @@
+public class CharacterContext {
+    private final int characterLocation;
+
+    public CharacterContext(int characterLocation) {
+        this.characterLocation = characterLocation;
+    }
+
+    public int getCharacterLocation() {
+        return characterLocation;
+    }
+}
