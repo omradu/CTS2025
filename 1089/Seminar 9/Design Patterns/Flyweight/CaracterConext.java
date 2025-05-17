42fb9e2a115a0f43c9832d7333b645aa8f437c3b
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-17 19:08:43 +0300

    Create CaracterConext.java

---
+++ 1089/Seminar 9/Design Patterns/Flyweight/CaracterConext.java
@@ -0,0 +1,13 @@
+public class CaracterConext {
+    private final int locatieLitera;
+
+    public CaracterConext (int locatieLitera) {
+
+        this.locatieLitera = locatieLitera;
+    }
+
+    public int getLocatie() {
+
+        return this.locatieLitera;
+    }
+}
