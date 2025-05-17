990c1544a925f0984df60556c0f0b0bf13db7d90
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-05-17 19:04:12 +0300

    Create ManagerM2.java

---
+++ 1107/Lab 9/Design Patterns/Chain/classes/ManagerM2.java
@@ -0,0 +1,18 @@
+public class ManagerM2 extends BugetApprover{
+    public static final int MAXIMUM_BUDGET = 1500;
+
+    @Override
+    public void approveBudget(float amount) {
+        if(amount < MAXIMUM_BUDGET) {
+            System.out.println("Budget approved by M2");
+        } else
+        {
+            if(nextApprover!=null) {
+                System.out.println("Budget not approved by M2, going to next level");
+                nextApprover.approveBudget(amount);
+            } else {
+                System.out.println("Budget rejected!");
+            }
+        }
+    }
+}
