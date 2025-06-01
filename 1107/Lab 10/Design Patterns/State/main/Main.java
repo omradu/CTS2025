1ef8f850e385fa402455c534ed196aecb9915f54
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-06-01 23:58:49 +0300

    Create Main.java

---
+++ 1107/Lab 10/Design Patterns/State/main/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pet pet = new Pet("Azorel");
+        pet.move();
+        pet.play(20);
+        pet.move();
+        pet.sleep(10);
+        pet.move();
+        pet.sleep(100);
+        pet.move();
+    }
+}
