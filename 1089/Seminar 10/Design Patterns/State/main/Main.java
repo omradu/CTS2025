aaa62938c4eda0a2ab1cb24c6e799c0e9069972b
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-06-02 00:13:04 +0300

    Create Main.java

---
+++ 1089/Seminar 10/Design Patterns/State/main/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Personaj personaj = new Personaj("Vasile");
+        personaj.misca();
+        personaj.seJoaca(70);
+        personaj.misca();
+        personaj.doarme(20);
+        personaj.misca();
+        personaj.doarme(100);
+        personaj.misca();
+    }
+}
