33dad0aa1ede788acb54cb6ce4a4112e0d134673
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-06-04 21:21:05 +0300

    Create IPersonValidation.java

---
+++ 1107/Lab 11/Testing/interfaces/IPersonValidation.java
@@ -0,0 +1,9 @@
+public interface IPersonValidation {
+    public static final int MIN_AGE = 14;
+    public static final int MAX_AGE = 99;
+    public static final int MIN_NAME_LENGTH = 3;
+    public static final int MAX_NAME_LENGTH = 200;
+    public static final int MIN_ADDRESS_LENGTH = 10;
+    public static final int MAX_ADDRESS_LENGTH = 500;
+    public static final int CNP_LENGTH = 13; 
+}
