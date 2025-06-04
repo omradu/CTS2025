b489c889ac3914209db171d13c152a23429f24c7
Maria Radu <68914986+omradu@users.noreply.github.com>
2025-06-04 21:10:27 +0300

    Create IValidariPersoana.java

---
+++ 1089/Seminar 11/Testing/interfete/IValidariPersoana.java
@@ -0,0 +1,9 @@
+public interface IValidariPersoana {
+    public static final int VARTSA_MIN = 14;
+    public static final int VARTSA_MAX = 99;
+    public static final int DIM_NUME_MIN = 3;
+    public static final int DIM_NUME_MAX = 200;
+    public static final int DIM_ADRESA_MIN = 10;
+    public static final int DIM_ADRESA_MAX = 500;
+    public static final int DIM_CNP = 13;
+}
